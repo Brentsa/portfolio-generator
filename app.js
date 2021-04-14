68d68313f41d199542d381d65b0f82729f15080b
Solidshots <seanbrent5@gmail.com>
2021-04-14 08:17:25 -0400

    Dynamically Generating HTML

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
+const fs = require('fs');
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (givenName, username) => {
-    return  `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${givenName}</h1>
-      <h2><a href="https://github.com/${username}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+fs.writeFile("index.html", generatePage(name, github), err => {
+  if(err) throw err;
 
-console.log(generatePage( name, github));
+  console.log("The file was created successfully! Check index.html for the site");
+});
