e3a616095325fdfc14cef6d0503b81d2abb433fb
Solidshots <seanbrent5@gmail.com>
2021-04-15 08:55:18 -0400

    Question sequence functioning

--- app.js
+++ app.js
@@ -41,9 +41,21 @@ const promptUser = () =>
       }
     },
     {
+      type: "confirm",
+      name: "confirmAbout",
+      message: "Would you like to enter some information about yourself for an 'About' section?",
+      default: true
+    },
+    {
       type: "input",
       name: "about",
       message: "Enter a description about yourself.",
+      when: ({confirmAbout}) =>{
+        if(confirmAbout) 
+          return true;
+        else 
+          return false;
+      }
     }
   ]);
 }
