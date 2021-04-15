3bb3fa0add12ffab67e14b4eede061dd2d6dd984
Solidshots <seanbrent5@gmail.com>
2021-04-15 07:48:30 -0400

    inquirer functioning

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const fs = require('fs');
-const generatePage = require("./src/page-template.js");
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(userName, github); 
 
-const [name, github] = profileDataArgs;
+// fs.writeFile("index.html", pageHTML, err => {
+//   if(err) throw err;
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-  if(err) throw err;
+//   console.log("The file was created successfully! Check index.html for the site");
+// });
 
-  console.log("The file was created successfully! Check index.html for the site");
-});
+
+function promptUser()
+{
+  inquirer.prompt([
+    {
+      type: "input", 
+      name: "name",
+      message: "What is your name?" 
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username?"
+    }
+  ])
+  .then(answer => console.log(answer));
+}
+
+promptUser();
