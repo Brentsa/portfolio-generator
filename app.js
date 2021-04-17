6a0f84d7618c1993114f86ee04be34611a87b876
Solidshots <seanbrent5@gmail.com>
2021-04-17 17:36:20 -0400

    working version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require("./src/page-template.js");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 
 const promptUser = () =>
@@ -124,20 +124,44 @@ const promptProject = portfolioData =>
   });
 }
 
-// promptUser()
-// .then(promptProject)
-// .then(portfolioData => {
-//   const pageHTML = generatePage(portfolioData); 
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData); 
 
-//   fs.writeFile("./index.html", pageHTML, err => {
-//     if(err) throw err;
+  fs.writeFile("./dist/index.html", pageHTML, err => {
+    if(err) throw err;
 
-//     console.log(`
-//   The file was created successfully! Check index.html for the site
-//     `);
-//   });
-// }); 
+    console.log(`The file was created successfully! Check index.html for the site`);
 
+    fs.copyFile("./src/style.css", "./dist/style.css", err => {
+      if(err) throw err;
+    
+      console.log(`The file style.css was copied into the dist folder properly`);
+    });
+  });
+}); 
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData=>{
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML=>{
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse=>{
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse=>{
+    console.log(copyFileResponse);
+  })
+  .catch(err =>{
+    console.log(err)
+  });
+
+/*
 var mockData = {
   name: 'Lernantino',
   github: 'lernantino',
@@ -186,10 +210,15 @@ var mockData = {
 
 const pageHTML = generatePage(mockData);
 
-fs.writeFile("./index.html", pageHTML, err => {
+fs.writeFile("./dist/index.html", pageHTML, err => {
   if(err) throw err;
 
-  console.log(`
-  The file was created successfully! Check index.html for the site
-  `);
+  console.log(`The file was created successfully! Check index.html for the site`);
+
+  fs.copyFile("./src/style.css", "./dist/style.css", err => {
+    if(err) throw err;
+  
+    console.log(`The file style.css was copied into the dist folder properly`);
+  });
 });
+*/
